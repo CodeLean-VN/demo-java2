2a2b3d4ec7706d43492a70836c768bfa042325f2
CodeLean-VN <codeleanvn@gmail.com>
2020-05-29 08:13:52 +0700

    example java2

--- src/main/java/bai1/Product.java
+++ src/main/java/bai1/Product.java
@@ -1,36 +1,84 @@
 package bai1;
 
-public class Product {
- 
-    //cac thuoc tinh
+/**
+ *
+ * @author VanTT
+ */
+public class Product implements DAO{
+    
     private String name;
     private double price;
     
     public Product(){
         this.name = "no name";
-        this.price = 1.0;
+        this.price = 12.5;
     }
     
-    public Product(String name, double price){
+    public Product(String n, double p){
+        this.name = n;
+        this.price = p;
+    }
+
+    /*
+     * Muc dich: tra ve ten cua san pham
+     *  
+     */
+    public String getName() {
+        return name;
+    }
+
+    /*
+     * Muc dich: tra ve ten cua san pham
+     *  
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /*
+     * Muc dich: thay doi ten cua san pham
+     * Dau vao: ten khac
+     *  
+     */
+    public void setName(String name) {
         this.name = name;
+    }
+
+    /*
+     * Muc dich: tra ve ten cua san pham
+     *  
+     */
+    public void setPrice(double price) {
         this.price = price;
     }
     
-    public String getName(){
-        return this.name;
+    public double getImportTax(){
+        return 0.1 * this.price;
     }
-    
-    //Muc dich: thiet dat ten moi cho ten cua san pham 
-    public void setName(String tenMoi){
-        this.name = tenMoi;
+
+    @Override
+    public String toString() {
+        return name + "; " + price + "; tax: " + getImportTax();
     }
-    
-    public double getPrice(){
-        return this.price;
+
+    @Override
+    public void insert() {
+        System.out.println("insert thanh cong!");
     }
-    
-    public void setPrice(double giaKhac){
-        this.price = giaKhac;
+
+    @Override
+    public void select() {
+        System.out.println("select thanh cong!");
     }
-    
+
+    @Override
+    public void update() {
+        System.out.println("update thanh cong!");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("delete thanh cong!");
+    }
+   
 }
--- src/main/java/bai1/TestProduct.java
+++ src/main/java/bai1/TestProduct.java
@@ -1,29 +1,72 @@
 package bai1;
 
-/**
- *
- * @author VanTT
- */
 public class TestProduct {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         
+        //c1
+        DAO sPCoThue1 = new Product("Nokia", 23.56);
+        System.out.println("Ten SP1: " + sPCoThue1);
+        System.out.println("Hien thi du lieu");
+        sPCoThue1.select();;
+        
+        //c2
+        Product sPCoThue2 = new Product("Sam sung", 25.67);
+        System.out.println("Ten SP2: " + sPCoThue2.getName());
+        double thueSP2 = sPCoThue2.getImportTax();
+        System.out.println("Thue SP2: " + thueSP2);
+        System.out.println("Chen db");
+        sPCoThue2.insert();
+        
+        //Tao san pham khong thue
+        DAO sPKhongThue1 = new NoTaxProduct("Dell", 12.56);
+        
+        Product sPKhongThue2 = new NoTaxProduct("Dasani", 5);
+        
+        NoTaxProduct sPKhongThue3 = new NoTaxProduct("casino", 2);
+        
+        
+        /*NoTaxProduct vanHoa = new NoTaxProduct("Am nhac", 23.56);
+        
+        
+        
+        ProductController pController = new ProductController();
+        
+        for(int i = 0; i < 3; i++){
+            System.out.println("Product " + (i+1) + ": ");
+            pController.inputProduct();
+        }
+        
+        pController.outputProduct();
+        */    
+            
+        /*
+        //1. Tao doi tuong Product ten la p1 
+        //su dung phuong thuc khoi tao ko tham so        
         Product p1 = new Product();
-        System.out.println(p1.getName() + ", " + p1.getPrice());
         
-        //doi ten p1
-        p1.setName("Mobile");
-        System.out.println("In ten sau khi doi: " + p1.getName());
+        System.out.println("Ten luc dau: " + p1.getName());
         
-        //doi gia p1
-        p1.setPrice(23.556);
-        System.out.println("In gia sau khi doi: " + p1.getPrice());
+        //2. Doi ten cua doi tuong p1 thanh "Nokia"
+        p1.setName("Nokia");
+        
+        //3. Hien thi ten va gia cua p1
+        System.out.println("Ten sau khi doi: " + p1.getName() 
+                                                + "; gia: " + p1.getPrice());
+
+        
+        //4. Hien thi thue cua p1
+        double thueP1 = p1.getImportTax();
+        System.out.println("Thue cua p1: " + thueP1);
+        
+        //5. Tao doi tuong Product ten la p2
+        //su dung phuong thuc khoi tao voi 2 tham so 
+        //name = "sam sung"; price = 23.56
         
         
-        Product p2 = new Product("Sam sung", 34.677);
-        System.out.println(p2.getName() + "," + p2.getPrice());
+        //6. Doi gia cho p2 thanh 56.89
         
-        //doi ten p2 thanh nokia
-        p2.setName("nokia");
-        System.out.println("ten p2 bi doi: " + p2.getName());
+        //7. Hien thi ten va gia cua p2
+       */
+        
     }
 }
